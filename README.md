cc6502ac0d0033b17ddb53309d5be5698d148693
Kaan Darcey <kaan@tmmedia.ca>
2022-07-28 21:32:29 -0700

    master feat: initial setup, dependencies and style

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tensai
+
+Tensai is a Spaced Repetition System (SRS) to help users learn basic vocabulary in the Japanese language.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {Routes} from "react-router-dom";
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="about" element={<About />} /> */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
